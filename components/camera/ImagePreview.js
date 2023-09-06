da37383e7a071abd5a46856011082045e0bb1d11
klasnasman <klas.nasman@gmail.com>
2023-09-06 08:38:29 +0200

    camera + tab nav

---
+++ components/camera/ImagePreview.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
+import { Feather, Entypo } from "@expo/vector-icons";
+import * as MediaLibrary from "expo-media-library";
+import { navigate } from "../navigators/RootNavigation";
+
+const ImagePreview = ({ pictureUri, resetCamera }) => {
+  const handleApprove = async () => {
+    try {
+      console.log("Saving picture...");
+      const asset = await MediaLibrary.createAssetAsync(pictureUri);
+
+      const album = await MediaLibrary.getAlbumAsync("Expo");
+
+      if (album == null) {
+        console.log("Creating album...");
+        await MediaLibrary.createAlbumAsync("Expo", asset, false);
+      } else {
+        console.log("Adding asset to album...");
+        await MediaLibrary.addAssetsToAlbumAsync(asset, album.id, false);
+      }
+
+      console.log("Picture saved successfully.");
+
+      navigate("Profile");
+      resetCamera();
+    } catch (error) {
+      console.error("Error saving picture:", error);
+    }
+  };
+
+  const handleResetCamera = () => {
+    resetCamera();
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: pictureUri }} style={{ flex: 1 }} />
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity
+          style={styles.generalButton}
+          onPress={handleResetCamera}>
+          <Feather name="trash" size={24} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.generalButton} onPress={handleApprove}>
+          <Entypo name="check" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: 16,
+  },
+  generalButton: {
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 30,
+    width: 50,
+    height: 50,
+    marginRight: 5,
+  },
+});
+
+export default ImagePreview;
--- components/profile/Profile.js
+++ components/profile/Profile.js
@@ -1,23 +1,32 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, useRef } from "react";
 import {
   View,
   Text,
   Alert,
   TextInput,
   StyleSheet,
+  Image,
   TouchableOpacity,
+  KeyboardAvoidingView,
 } from "react-native";
+import * as MediaLibrary from "expo-media-library";
+import * as ImagePicker from "expo-image-picker";
 import { AuthContext } from "../context/AuthProvider";
 import ApiRailway from "../api/ApiRailway";
+import { Feather } from "@expo/vector-icons";
 
 const Profile = () => {
   const { accessToken, handleLogout } = useContext(AuthContext);
   const [userData, setUserData] = useState(null);
   const [newFirstname, setNewFirstname] = useState("");
   const [newLastname, setNewLastname] = useState("");
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [hasMediaPermission, setHasMediaPermission] = useState(null);
+  const inputRef = useRef();
 
   useEffect(() => {
     fetchUser();
+    checkMediaLibraryPermission();
   }, []);
 
   const fetchUser = async () => {
@@ -42,6 +51,7 @@ const Profile = () => {
       };
 
       setUserData(updatedUser);
+      inputRef.current.blur();
       console.log(updatedUser);
     } catch (error) {
       console.error("Error updating user:", error);
@@ -57,41 +67,103 @@ const Profile = () => {
       console.error("Error deleting user:", error);
     }
   };
+
+  const checkMediaLibraryPermission = async () => {
+    const mediaLibraryPermission = await MediaLibrary.getPermissionsAsync();
+    if (mediaLibraryPermission.granted) {
+      setHasMediaPermission(true);
+    } else {
+      setHasMediaPermission(false);
+    }
+  };
+
+  const pickImage = async () => {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [1, 1],
+        quality: 1,
+      });
+
+      if (!result.canceled) {
+        setSelectedImage(result.assets[0].uri);
+      }
+    } catch (error) {
+      console.error("Error in pickImage function:", error);
+    }
+  };
+
   return (
     <View style={styles.profileContainer}>
-      {userData && (
-        <View style={styles.userContainer}>
-          <Text style={styles.userDetailText}>
-            {userData.firstname || "First Name"}{" "}
-          </Text>
-          <Text style={styles.userDetailText}>
-            {userData.lastname || "Last Name"}
-          </Text>
+      <View style={styles.profileContainerInner}>
+        <View style={styles.imageContainer}>
+          {selectedImage ? (
+            <Image
+              source={{ uri: selectedImage }}
+              style={styles.profileImage}
+            />
+          ) : (
+            <Image
+              source={require("../../assets/placeholder.png")}
+              style={styles.profileImage}
+            />
+          )}
+          <TouchableOpacity style={styles.pickImageButton} onPress={pickImage}>
+            <Feather name="plus-circle" size={24} color="black" />
+          </TouchableOpacity>
         </View>
-      )}
-      <View style={styles.inputFieldsContainer}>
-        <TextInput
-          style={styles.inputField}
-          placeholder="New First Name"
-          value={newFirstname}
-          onChangeText={setNewFirstname}
-        />
-        <TextInput
-          style={styles.inputField}
-          placeholder="New Last Name"
-          value={newLastname}
-          onChangeText={setNewLastname}
-        />
+        {userData && (
+          <View style={styles.inputFieldsContainer}>
+            <TextInput
+              style={[
+                styles.inputField,
+                !userData.firstname && { color: "black" },
+              ]}
+              placeholder={
+                userData.firstname ? userData.firstname : "Add First Name"
+              }
+              value={newFirstname}
+              onChangeText={setNewFirstname}
+              ref={inputRef}
+              placeholderTextColor={"gray"}
+            />
+            <TextInput
+              style={[
+                styles.inputField,
+                !userData.lastname && { color: "black" },
+              ]}
+              placeholder={
+                userData.lastname ? userData.lastname : "Add First Name"
+              }
+              value={newLastname}
+              onChangeText={setNewLastname}
+              ref={inputRef}
+              placeholderTextColor={"gray"}
+            />
+          </View>
+        )}
       </View>
-
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} onPress={() => updateName()}>
-          <Text>Update User</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => deleteUser()}>
-          <Text>Delete Account</Text>
-        </TouchableOpacity>
-      </View>
+      <KeyboardAvoidingView
+        keyboardVerticalOffset={120}
+        enabled={true}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.keyboardAvoidingContainer}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={[styles.blueButton, styles.button, styles.halfWidth]}
+            onPress={() => updateName()}>
+            <Feather name="user-check" size={24} color="white" />
+            <Text style={styles.signOutButtonText}>Update User</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.redButton, styles.button, styles.halfWidth]}
+            onPress={() => deleteUser()}>
+            <Feather name="user-minus" size={24} color="white" />
+            <Text style={styles.signOutButtonText}>Delete Account</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
     </View>
   );
 };
@@ -99,7 +171,32 @@ const Profile = () => {
 const styles = StyleSheet.create({
   profileContainer: {
     flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: 32,
     backgroundColor: "white",
+  },
+  profileContainerInner: {
+    flex: 1,
+  },
+  imageContainer: {
+    position: "relative",
+    alignItems: "center",
+  },
+  pickImageButton: {
+    position: "absolute",
+    bottom: 0,
+    justifyContent: "center",
+    alignItems: "center",
+    left: "50%",
+    transform: [{ translateX: 20 }],
+    borderRadius: 50,
+    backgroundColor: "#f1f1f0",
+    overflow: "hidden",
+  },
+  userContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderColor: "#EDEDED",
   },
   inputFieldsContainer: {
     gap: 16,
@@ -109,17 +206,47 @@ const styles = StyleSheet.create({
   inputField: {
     paddingHorizontal: 10,
     backgroundColor: "#f1f1f0",
+    borderRadius: 15,
+    borderWidth: 1,
     height: 30,
+    borderColor: "#EDEDED",
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 8,
+    borderWidth: 1,
+    marginLeft: "auto",
+    marginRight: "auto",
+    backgroundColor: "#ddd",
   },
   buttonContainer: {
+    flexDirection: "row-reverse",
     gap: 8,
+  },
+  halfWidth: {
+    flex: 1,
+    width: "50%",
+    flexDirection: "row",
   },
   button: {
     alignItems: "center",
     justifyContent: "center",
     gap: 8,
+    flexWrap: "nowrap",
+    borderRadius: 22,
     height: 44,
     marginBottom: 32,
+  },
+  redButton: {
+    backgroundColor: "#FF0000",
+  },
+  blueButton: {
+    backgroundColor: "#1982FC",
+  },
+  signOutButtonText: {
+    color: "#fff",
   },
 });
 
