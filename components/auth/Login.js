f38942a70885536cdad00ee3c147f503addb48d8
klasnasman <klas.nasman@gmail.com>
2023-09-07 16:19:51 +0200

    keyboard & styling done

--- components/auth/Login.js
+++ components/auth/Login.js
@@ -5,7 +5,9 @@ import {
   TouchableOpacity,
   Text,
   StyleSheet,
+  KeyboardAvoidingView,
 } from "react-native";
+import { Feather } from "@expo/vector-icons";
 import { AuthContext } from "../context/AuthProvider";
 import ApiRailway from "../api/ApiRailway";
 
@@ -21,8 +23,6 @@ export default function Login({ navigation }) {
       const accessToken = response.data.accessToken;
       const userId = response.data._id;
       handleLogin(accessToken, userId);
-      console.log("login.js - accesstoken:", accessToken);
-      console.log("login.js - userId:", userId);
     } catch (error) {
       console.error("Login Error:", error);
       if (error.response && error.response.data) {
@@ -32,48 +32,98 @@ export default function Login({ navigation }) {
   };
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        onChangeText={setUsername}
-        value={username}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry
-        onChangeText={setPassword}
-        value={password}
-      />
-      <TouchableOpacity onPress={() => handleLoginPress()}>
-        <Text>Sign In</Text>
-      </TouchableOpacity>
+    <View style={styles.welcome}>
+      <View style={styles.logo}>
+        <Feather name="smile" size={60} color="#59c639" />
+      </View>
+      <KeyboardAvoidingView
+        enabled={true}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.keyboardAvoidingContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          onChangeText={setUsername}
+          value={username}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+          onChangeText={setPassword}
+          value={password}
+        />
+        <TouchableOpacity
+          style={styles.signOutButton}
+          onPress={() => handleLoginPress()}>
+          <Feather name="log-in" size={24} color="#fff999" />
+          <Text style={styles.signOutButtonText}>Sign In</Text>
+        </TouchableOpacity>
 
-      <Text
-        onPress={() => {
-          setUsername("");
-          setPassword("");
-          setErrorMessage("");
-          navigation.navigate("Create Account");
-        }}>
-        Don't have an account? Register here
-      </Text>
-      {errorMessage && <Text>{errorMessage}</Text>}
+        <Text
+          style={styles.linkText}
+          onPress={() => {
+            setUsername("");
+            setPassword("");
+            setErrorMessage("");
+            navigation.navigate("Create Account");
+          }}>
+          Don't have an account? Register here
+        </Text>
+        {errorMessage && <Text style={styles.errorText}>{errorMessage}</Text>}
+      </KeyboardAvoidingView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  keyboardAvoidingContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
     gap: 16,
+    paddingHorizontal: 16,
     backgroundColor: "white",
+  },
+  welcome: {
+    flex: 1,
+    position: "relative",
+  },
+  logo: {
+    position: "absolute",
+    top: 56,
+    right: "50%",
+    transform: [{ translateX: 30 }],
+    zIndex: "999",
   },
   input: {
     width: "100%",
-    backgroundColor: "grey",
+    height: 30,
+    paddingHorizontal: 10,
+    backgroundColor: "#f1f1f0",
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: "#EDEDED",
+  },
+  errorText: {
+    color: "red",
+    marginTop: 10,
+  },
+  signOutButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#1982FC",
+    borderRadius: 22,
+    height: 44,
+    paddingVertical: 10,
+    marginBottom: 16,
+    width: "100%",
+    paddingVertical: 10,
+  },
+  signOutButtonText: {
+    marginLeft: 8,
+    fontSize: 16,
+    color: "#fff",
   },
 });
--- components/auth/Register.js
+++ components/auth/Register.js
@@ -5,7 +5,9 @@ import {
   TouchableOpacity,
   StyleSheet,
   Text,
+  KeyboardAvoidingView,
 } from "react-native";
+import { Feather } from "@expo/vector-icons";
 import ApiRailway from "../api/ApiRailway";
 
 export default function Register({ navigation }) {
@@ -16,7 +18,6 @@ export default function Register({ navigation }) {
   const handleRegisterPress = async () => {
     try {
       await ApiRailway.register(username, password);
-      console.log("handleRegisterPress success");
       navigation.navigate("Sign In");
     } catch (error) {
       if (error.response && error.response.status === 409) {
@@ -32,38 +33,76 @@ export default function Register({ navigation }) {
   };
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        onChangeText={setUsername}
-        value={username}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry
-        onChangeText={setPassword}
-        value={password}
-      />
-      <TouchableOpacity onPress={() => handleRegisterPress()}>
-        <Text>Create Account</Text>
-      </TouchableOpacity>
-      {errorMessage && <Text>{errorMessage}</Text>}
-    </View>
+    <KeyboardAvoidingView
+      enabled={true}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.keyboardAvoidingContainer}>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          onChangeText={setUsername}
+          value={username}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+          onChangeText={setPassword}
+          value={password}
+        />
+        <TouchableOpacity
+          style={styles.signOutButton}
+          onPress={() => handleRegisterPress()}>
+          <Feather name="user-plus" size={24} color="#fff999" />
+          <Text style={styles.signOutButtonText}>Create Account</Text>
+        </TouchableOpacity>
+        {errorMessage && <Text style={styles.errorText}>{errorMessage}</Text>}
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
+  keyboardAvoidingContainer: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
     gap: 16,
+    paddingHorizontal: 16,
     backgroundColor: "white",
   },
   input: {
     width: "100%",
-    backgroundColor: "grey",
+    height: 30,
+    paddingHorizontal: 10,
+    backgroundColor: "#f1f1f0",
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: "#EDEDED",
+  },
+  errorText: {
+    color: "red",
+    marginTop: 10,
+  },
+  signOutButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#1982FC",
+    borderRadius: 22,
+    height: 44,
+    paddingVertical: 10,
+    marginBottom: 16,
+    width: "100%",
+    paddingVertical: 10,
+  },
+  signOutButtonText: {
+    marginLeft: 8,
+    fontSize: 16,
+    color: "#fff",
   },
 });
